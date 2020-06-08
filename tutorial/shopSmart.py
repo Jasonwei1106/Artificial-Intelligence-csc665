81b9ef8c20022b90a026d7591c5bbde7b36c8702
Jason Wei <jason.wei.1106@gmail.com>
2020-06-08 13:48:07 -0700

    upload assignment 1 and 2

---
+++ tutorial/shopSmart.py
@@ -0,0 +1,56 @@
+# shopSmart.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Here's the intended output of this script, once you fill it in:
+
+Welcome to shop1 fruit shop
+Welcome to shop2 fruit shop
+For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
+For orders:  [('apples', 3.0)] best shop is shop2
+"""
+from __future__ import print_function
+import shop
+
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """
+    bestshop = fruitShops[0]  #set the default shop
+    lowestprice=0
+    for shops in fruitShops:  #loop through fruitshops
+        totalprice = 0   #set totalprice to 0 each time we have a new shop
+        for orders in orderList:   #loop through orderlist
+            totalprice += (shops.getCostPerPound(orders[0]) * orders[1])   #adds up all the items' total price
+        if lowestprice == 0:
+            lowestprice = totalprice    #the first loop set the default lowestprice
+        if lowestprice > totalprice:
+            bestshop = shops   #change the shop if the shop has a lower price.
+            lowestprice = totalprice
+    return bestshop
+
+
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orders = [('apples', 1.0), ('oranges', 3.0)]
+    dir1 = {'apples': 2.0, 'oranges': 1.0}
+    shop1 = shop.FruitShop('shop1', dir1)
+    dir2 = {'apples': 1.0, 'oranges': 5.0}
+    shop2 = shop.FruitShop('shop2', dir2)
+    shops = [shop1, shop2]
+    print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    orders = [('apples', 3.0)]
+    print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
